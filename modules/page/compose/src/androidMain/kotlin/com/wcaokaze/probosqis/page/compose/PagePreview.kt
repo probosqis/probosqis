77e2b0b30cb1cdbdbd5aae68bda24ff168c01cbb
wcaokaze <wcaokaze@gmail.com>
2023-02-14 03:18:40 +0900

    PageのComposableを実装

---
+++ modules/page/compose/src/androidMain/kotlin/com/wcaokaze/probosqis/page/compose/PagePreview.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2023 wcaokaze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.wcaokaze.probosqis.page.compose
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.wcaokaze.probosqis.page.core.Page
+
+private class PreviewPage : Page()
+
+@Preview
+@Composable
+private fun PagePreview() {
+   val metadataCollection = PageMetadataCollection(
+      listOf(
+         pageMetadata<PreviewPage> { Text("PreviewPage") },
+      )
+   )
+
+   val page = PreviewPage()
+   Page(page, metadataCollection)
+}
