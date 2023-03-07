49c02790441b9ce93da7486cdb0a5d2148f92382
wcaokaze <wcaokaze@gmail.com>
2023-03-08 00:39:18 +0900

    ColumnBoardの読み込みと失敗時のデフォルト値をテスト可能な関数に抽出してテスト書いた

--- modules/app/src/commonMain/kotlin/com/wcaokaze/probosqis/app/Probosqis.kt
+++ modules/app/src/commonMain/kotlin/com/wcaokaze/probosqis/app/Probosqis.kt
@@ -20,25 +20,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import com.wcaokaze.probosqis.cache.core.WritableCache
 import com.wcaokaze.probosqis.page.compose.ColumnBoard
 import com.wcaokaze.probosqis.page.compose.ColumnBoardState
 import com.wcaokaze.probosqis.page.core.Column
 import com.wcaokaze.probosqis.page.core.ColumnBoard
+import com.wcaokaze.probosqis.page.perpetuation.ColumnBoardRepository
 import kotlinx.collections.immutable.persistentListOf
 
 @Composable
 fun Probosqis(di: DI) {
    val columnBoardState = remember {
-      val columnBoardRepository = di.columnBoardRepository
-      val columnBoardCache = try {
-         columnBoardRepository.loadColumnBoard()
-      } catch (e: Exception) {
-         val columnBoard = ColumnBoard(
-            columns = createDefaultColumns()
-         )
-         columnBoardRepository.saveColumnBoard(columnBoard)
-      }
-
+      val columnBoardCache = loadColumnBoardOrDefault(di.columnBoardRepository)
       ColumnBoardState(columnBoardCache, di.allPageMetadata)
    }
 
@@ -46,6 +39,19 @@ fun Probosqis(di: DI) {
       columnBoardState,
       modifier = Modifier.fillMaxSize()
    )
+}
+
+internal fun loadColumnBoardOrDefault(
+   columnBoardRepository: ColumnBoardRepository
+): WritableCache<ColumnBoard> {
+   return try {
+      columnBoardRepository.loadColumnBoard()
+   } catch (e: Exception) {
+      val columnBoard = ColumnBoard(
+         columns = createDefaultColumns()
+      )
+      columnBoardRepository.saveColumnBoard(columnBoard)
+   }
 }
 
 private fun createDefaultColumns(): List<Column> {
