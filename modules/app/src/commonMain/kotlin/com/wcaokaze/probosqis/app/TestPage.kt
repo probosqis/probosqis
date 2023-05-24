f04c440e7c1417ae1b7433c5641b91f8679aa43f
wcaokaze <wcaokaze@gmail.com>
2023-05-25 03:54:19 +0900

    compose.materialをmaterial3に移行

--- modules/app/src/commonMain/kotlin/com/wcaokaze/probosqis/app/TestPage.kt
+++ modules/app/src/commonMain/kotlin/com/wcaokaze/probosqis/app/TestPage.kt
@@ -18,8 +18,8 @@ package com.wcaokaze.probosqis.app
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
