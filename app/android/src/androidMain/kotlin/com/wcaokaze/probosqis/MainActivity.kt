85ba39cf088aea87007ba865de8c7690912a6947
wcaokaze <wcaokaze@gmail.com>
2024-04-16 02:39:25 +0900

    setUpSystemBarsを削除、enableEdgeToEdgeを使うようにした

--- app/android/src/androidMain/kotlin/com/wcaokaze/probosqis/MainActivity.kt
+++ app/android/src/androidMain/kotlin/com/wcaokaze/probosqis/MainActivity.kt
@@ -16,37 +16,32 @@
 
 package com.wcaokaze.probosqis
 
-import android.content.res.Configuration
-import android.os.Build
 import android.os.Bundle
-import android.view.View
-import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.platform.LocalContext
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsControllerCompat
 import com.wcaokaze.probosqis.app.Probosqis
 import com.wcaokaze.probosqis.app.ProbosqisState
 import com.wcaokaze.probosqis.app.TestNotePage
 import com.wcaokaze.probosqis.app.TestPage
 import com.wcaokaze.probosqis.app.TestTimelinePage
 import com.wcaokaze.probosqis.app.pagedeck.AndroidPageDeckRepository
+import com.wcaokaze.probosqis.app.pagedeck.AndroidPageStackRepository
+import com.wcaokaze.probosqis.app.pagedeck.pageSerializer
 import com.wcaokaze.probosqis.app.testNotePageComposable
 import com.wcaokaze.probosqis.app.testPageComposable
 import com.wcaokaze.probosqis.app.testTimelinePageComposable
-import com.wcaokaze.probosqis.app.pagedeck.AndroidPageStackRepository
-import com.wcaokaze.probosqis.app.pagedeck.pageSerializer
 import kotlinx.collections.immutable.persistentListOf
 
 class MainActivity : ComponentActivity() {
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
 
-      setUpSystemBars()
+      enableEdgeToEdge()
 
       setContent {
          val context = LocalContext.current
@@ -80,64 +75,6 @@ class MainActivity : ComponentActivity() {
          }
 
          Probosqis(probosqisState)
-      }
-   }
-
-   private fun isDarkMode(): Boolean {
-      return (resources.configuration.uiMode and
-            Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
-   }
-
-   private fun setUpSystemBars() {
-      WindowCompat.setDecorFitsSystemWindows(window, false)
-
-      val view = findViewById<View>(android.R.id.content)
-      val windowInsetsController = WindowInsetsControllerCompat(window, view)
-
-      val isDarkMode = isDarkMode()
-      setUpStatusBar(windowInsetsController, isDarkMode)
-      setUpNavigationBar(windowInsetsController, isDarkMode)
-   }
-
-   private fun setUpStatusBar(
-      windowInsetsController: WindowInsetsControllerCompat,
-      isDarkMode: Boolean
-   ) {
-      if (Build.VERSION.SDK_INT >= 23) {
-         window.statusBarColor = 0
-         windowInsetsController.isAppearanceLightStatusBars = !isDarkMode
-      } else {
-         // 22以前は透明にはできるもののアイコンの色を変更できないため半透明にする
-         window.addFlags(
-            @Suppress("DEPRECATION")
-            WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
-         )
-      }
-   }
-
-   private fun setUpNavigationBar(
-      windowInsetsController: WindowInsetsControllerCompat,
-      isDarkMode: Boolean
-   ) {
-      when {
-         Build.VERSION.SDK_INT >= 29 -> {
-            // 29以降はジェスチャーナビゲーションのとき透明、
-            // 3ボタンナビゲーションのときはいい感じの半透明にしてくれる
-            window.navigationBarColor = 0
-            windowInsetsController.isAppearanceLightNavigationBars = !isDarkMode
-         }
-         Build.VERSION.SDK_INT >= 26 -> {
-            window.navigationBarColor =
-               if (isDarkMode) { 0x80000000.toInt() } else { 0xccffffff.toInt() }
-            windowInsetsController.isAppearanceLightNavigationBars = !isDarkMode
-         }
-         else -> {
-            // 25以前は透明にはできるもののアイコンの色を変更できないため半透明にする
-            window.addFlags(
-               @Suppress("DEPRECATION")
-               WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION
-            )
-         }
       }
    }
 }
