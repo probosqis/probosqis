24551a0ef07e7e0aa581f2d15d532d1d86347ca4
wcaokaze <wcaokaze@gmail.com>
2024-04-23 01:40:02 +0900

    ジェスチャーナビゲーションのときナビゲーションバーの背景を透明にした

--- app/android/src/androidMain/kotlin/com/wcaokaze/probosqis/MainActivity.kt
+++ app/android/src/androidMain/kotlin/com/wcaokaze/probosqis/MainActivity.kt
@@ -23,8 +23,11 @@ import androidx.activity.SystemBarStyle
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -32,6 +35,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.compositeOver
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
 import com.wcaokaze.probosqis.app.Probosqis
 import com.wcaokaze.probosqis.app.ProbosqisState
 import com.wcaokaze.probosqis.app.TestNotePage
@@ -48,7 +56,7 @@ import kotlinx.collections.immutable.persistentListOf
 
 class MainActivity : ComponentActivity() {
    override fun onCreate(savedInstanceState: Bundle?) {
-      enableEdgeToEdge(colorScheme = null)
+      initializeEdgeToEdge()
       super.onCreate(savedInstanceState)
 
       setContent {
@@ -83,40 +91,77 @@ class MainActivity : ComponentActivity() {
                probosqisState.pageDeckState.activePageStackState.finishPage()
             }
 
-            val colorScheme = MaterialTheme.colorScheme
-
-            LaunchedEffect(colorScheme) {
-               enableEdgeToEdge(colorScheme)
-            }
+            NavigationBarController()
 
             Probosqis(probosqisState)
          }
       }
    }
 
-   private fun enableEdgeToEdge(colorScheme: ColorScheme?) {
-      val isDarkTheme = (
-            resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
-         ) == Configuration.UI_MODE_NIGHT_YES
+   @Composable
+   private fun NavigationBarController() {
+      val colorScheme = MaterialTheme.colorScheme
+      val navigationBarsInsets = WindowInsets.navigationBars
+      val density = LocalDensity.current
+      val layoutDirection = LocalLayoutDirection.current
 
-      var lightNavigationBarScrim = Color.White
-      var darkNavigationBarScrim = Color(0x1B, 0x1B, 0x1B)
+      LaunchedEffect(colorScheme, navigationBarsInsets, layoutDirection) {
+         updateSystemBarColors(colorScheme, navigationBarsInsets, density, layoutDirection)
+      }
+   }
 
-      if (colorScheme == null) {
-         lightNavigationBarScrim = lightNavigationBarScrim.copy(alpha = 0.9f)
-         darkNavigationBarScrim = darkNavigationBarScrim.copy(alpha = 0.5f)
+   private fun isDarkTheme() = (
+         resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+      ) == Configuration.UI_MODE_NIGHT_YES
+
+   private val lightNavigationBarBackground = Color.White
+   private val darkNavigationBarBackground = Color(0x1B, 0x1B, 0x1B)
+
+   private fun initializeEdgeToEdge() {
+      val lightScrim = lightNavigationBarBackground.copy(alpha = 0.9f)
+      val darkScrim = darkNavigationBarBackground.copy(alpha = 0.5f)
+
+      enableEdgeToEdge(
+         navigationBarStyle = if (isDarkTheme()) {
+            SystemBarStyle.dark(darkScrim.toArgb())
+         } else {
+            SystemBarStyle.light(lightScrim.toArgb(), darkScrim.toArgb())
+         }
+      )
+   }
+
+   private fun updateSystemBarColors(
+      colorScheme: ColorScheme,
+      navigationBarsInsets: WindowInsets,
+      density: Density,
+      layoutDirection: LayoutDirection
+   ) {
+      val lightNavigationBarScrim: Color
+      val darkNavigationBarScrim:  Color
+
+      // ナビゲーションバーが画面下部にあって十分に低い場合透明にする
+      // 具体的には2ボタン、3ボタンでは半透明、ジェスチャーナビゲーションでは透明
+      val shouldTransparentNavigationBar =
+         navigationBarsInsets.getBottom(density) <= with (density) { 32.dp.toPx() }
+               && navigationBarsInsets.getLeft (density, layoutDirection) <= 0
+               && navigationBarsInsets.getTop  (density)                  <= 0
+               && navigationBarsInsets.getRight(density, layoutDirection) <= 0
+
+      if (shouldTransparentNavigationBar) {
+         lightNavigationBarScrim = Color.Transparent
+         darkNavigationBarScrim  = Color.Transparent
       } else {
-         lightNavigationBarScrim = lightNavigationBarScrim.copy(alpha = 0.9f)
+         lightNavigationBarScrim = lightNavigationBarBackground.copy(alpha = 0.9f)
             .compositeOver(colorScheme.primaryContainer)
             .copy(alpha = 0.9f)
 
-         darkNavigationBarScrim = darkNavigationBarScrim.copy(alpha = 0.9f)
+         darkNavigationBarScrim = darkNavigationBarBackground.copy(alpha = 0.9f)
             .compositeOver(colorScheme.primaryContainer)
             .copy(alpha = 0.8f)
       }
 
       enableEdgeToEdge(
-         navigationBarStyle = if (isDarkTheme) {
+         navigationBarStyle = if (isDarkTheme()) {
             SystemBarStyle.dark(darkNavigationBarScrim.toArgb())
          } else {
             SystemBarStyle.light(
