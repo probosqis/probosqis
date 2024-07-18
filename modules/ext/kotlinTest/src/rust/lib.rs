c6b6199092dad51b0c370a9b19b1c8cc6a945269
wcaokaze <wcaokaze@gmail.com>
2024-07-19 01:40:23 +0900

    added: ext-kotlintest::get_vm() -> JavaVM

--- modules/ext/kotlinTest/src/rust/lib.rs
+++ modules/ext/kotlinTest/src/rust/lib.rs
@@ -13,4 +13,28 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+use std::ptr::null_mut;
 
+use jni::JavaVM;
+use jni::sys::{JNI_GetCreatedJavaVMs, JNI_OK, jsize};
+
+#[cfg(feature="jni-test")]
+pub(crate) fn get_vm() -> JavaVM {
+   let mut buf: [*mut jni::sys::JavaVM; 4] = [null_mut(); 4];
+   let mut vm_count = 0;
+
+   let result_code = unsafe {
+      JNI_GetCreatedJavaVMs(
+         &mut buf as *mut _,
+         buf.len() as jsize,
+         &mut vm_count as *mut _
+      )
+   };
+
+   if result_code != JNI_OK { panic!(); }
+   if vm_count != 1 { panic!(); }
+
+   unsafe {
+      JavaVM::from_raw(buf[0]).unwrap()
+   }
+}
