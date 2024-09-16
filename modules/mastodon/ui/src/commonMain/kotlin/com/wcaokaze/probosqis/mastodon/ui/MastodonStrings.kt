6b4ed6a546bfa9c5e041df5030d2f445aa4cafa2
wcaokaze <wcaokaze@gmail.com>
2024-09-16 23:24:56 +0900

    URL入力ページのComposableを作成

---
+++ modules/mastodon/ui/src/commonMain/kotlin/com/wcaokaze/probosqis/mastodon/ui/MastodonStrings.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2024 wcaokaze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.wcaokaze.probosqis.mastodon.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import com.wcaokaze.probosqis.resources.LocalLanguage
+import com.wcaokaze.probosqis.resources.Strings
+import com.wcaokaze.probosqis.resources.appName
+
+interface MastodonStrings {
+   class AuthUrlInput(
+      val appBar: String,
+      val description: String,
+      val startAuthButton: String,
+   )
+
+   val authUrlInput: AuthUrlInput
+}
+
+val Strings.Companion.Mastodon: MastodonStrings
+   @Composable
+   @ReadOnlyComposable
+   get() = when (LocalLanguage.current) {
+      Strings.Language.ENGLISH -> object : MastodonStrings {
+         override val authUrlInput = MastodonStrings.AuthUrlInput(
+            appBar = "Add an account",
+            description = "Add an existing account to $appName.",
+            startAuthButton = "GO",
+         )
+      }
+
+      Strings.Language.JAPANESE -> object : MastodonStrings {
+         override val authUrlInput = MastodonStrings.AuthUrlInput(
+            appBar = "アカウントを追加",
+            description = "作成済みのアカウントを${appName}に追加します",
+            startAuthButton = "GO",
+         )
+      }
+   }
--- modules/mastodon/ui/src/commonMain/kotlin/com/wcaokaze/probosqis/mastodon/ui/auth/urlinput/UrlInputPage.kt
+++ modules/mastodon/ui/src/commonMain/kotlin/com/wcaokaze/probosqis/mastodon/ui/auth/urlinput/UrlInputPage.kt
@@ -16,30 +16,91 @@
 
 package com.wcaokaze.probosqis.mastodon.ui.auth.urlinput
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import com.wcaokaze.probosqis.capsiqum.page.PageStateFactory
+import com.wcaokaze.probosqis.mastodon.ui.Mastodon
 import com.wcaokaze.probosqis.page.PPage
 import com.wcaokaze.probosqis.page.PPageComposable
 import com.wcaokaze.probosqis.page.PPageState
+import com.wcaokaze.probosqis.resources.Strings
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.serializer
 
 @Serializable
 @SerialName("com.wcaokaze.probosqis.mastodon.ui.auth.urlinput")
 class UrlInputPage : PPage()
 
 @Stable
-class UrlInputPageState : PPageState() {
+class UrlInputPageState(stateSaver: StateSaver) : PPageState() {
+   var inputUrl: String by stateSaver.save("inputUrl", String.serializer()) { "" }
 }
 
 val urlInputPageComposable = PPageComposable<UrlInputPage, UrlInputPageState>(
-   PageStateFactory { _, _, _ -> UrlInputPageState() },
-   content = { _, _, _ ->
-   },
+   PageStateFactory { _, _, stateSaver -> UrlInputPageState(stateSaver) },
    header = { _, _ ->
+      Text(
+         Strings.Mastodon.authUrlInput.appBar,
+         maxLines = 1,
+         overflow = TextOverflow.Ellipsis
+      )
    },
-   footer = { _, _ ->
+   content = { _, state, _ ->
+      Column(
+         modifier = Modifier
+            .verticalScroll(rememberScrollState())
+            .padding(16.dp)
+      ) {
+         Text(
+            Strings.Mastodon.authUrlInput.description,
+            modifier = Modifier.padding(vertical = 8.dp)
+         )
+
+         Spacer(modifier = Modifier.height(16.dp))
+
+         TextField(
+            state.inputUrl,
+            onValueChange = { newValue ->
+               state.inputUrl = newValue
+            },
+            placeholder = {
+               Text("https://mastodon.social/")
+            },
+            singleLine = true,
+            modifier = Modifier
+               .padding(vertical = 8.dp)
+               .fillMaxWidth()
+         )
+
+         Button(
+            onClick = {},
+            shape = ButtonDefaults.filledTonalShape,
+            colors = ButtonDefaults.filledTonalButtonColors(),
+            elevation = ButtonDefaults.filledTonalButtonElevation(),
+            modifier = Modifier.align(Alignment.End)
+         ) {
+            Text(Strings.Mastodon.authUrlInput.startAuthButton)
+         }
+      }
    },
+   footer = null,
    pageTransitions = {
    }
 )
