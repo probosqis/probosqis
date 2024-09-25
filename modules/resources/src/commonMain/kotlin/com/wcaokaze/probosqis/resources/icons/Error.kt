bc54f298168eb9e1cd50d97a8f7a332098661316
wcaokaze <wcaokaze@gmail.com>
2024-09-26 01:51:39 +0900

    added: Icons.Filled.Error

--- modules/resources/src/commonMain/kotlin/com/wcaokaze/probosqis/resources/icons/Error.kt
+++ modules/resources/src/commonMain/kotlin/com/wcaokaze/probosqis/resources/icons/Error.kt
@@ -20,39 +20,68 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.materialIcon
 import androidx.compose.material.icons.materialPath
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.PathBuilder
+
+private fun PathBuilder.exclamation() {
+   moveTo(11.0f, 15.0f)
+   horizontalLineToRelative(2.0f)
+   verticalLineToRelative(2.0f)
+   horizontalLineToRelative(-2.0f)
+   close()
+   moveTo(11.0f, 7.0f)
+   horizontalLineToRelative(2.0f)
+   verticalLineToRelative(6.0f)
+   horizontalLineToRelative(-2.0f)
+   close()
+}
+
+private fun PathBuilder.outerCircle() {
+   moveTo(12.0f, 2.0f)
+   curveToRelative(-5.53f, 0.0f, -10.0f, 4.47f, -10.0f, 10.0f)
+   reflectiveCurveToRelative(4.47f, 10.0f, 10.0f, 10.0f)
+   reflectiveCurveToRelative(10.0f, -4.47f, 10.0f, -10.0f)
+   reflectiveCurveToRelative(-4.47f, -10.0f, -10.0f, -10.0f)
+   close()
+}
+
+private fun PathBuilder.innerCircle() {
+   moveTo(12.0f, 4.0f)
+   curveToRelative(4.42f, 0.0f, 8.0f, 3.58f, 8.0f, 8.0f)
+   reflectiveCurveToRelative(-3.58f, 8.0f, -8.0f, 8.0f)
+   reflectiveCurveToRelative(-8.0f, -3.58f, -8.0f, -8.0f)
+   reflectiveCurveToRelative(3.58f, -8.0f, 8.0f, -8.0f)
+   close()
+}
 
 @Suppress("UnusedReceiverParameter")
 val Icons.Outlined.Error: ImageVector get() {
-   if (_error != null) { return _error!! }
+   if (_outlinedError != null) { return _outlinedError!! }
 
-   _error = materialIcon("Filled.Error") {
+   _outlinedError = materialIcon("Outlined.Error") {
       materialPath {
-         moveTo(11.0f, 15.0f)
-         horizontalLineToRelative(2.0f)
-         verticalLineToRelative(2.0f)
-         horizontalLineToRelative(-2.0f)
-         close()
-         moveTo(11.0f, 7.0f)
-         horizontalLineToRelative(2.0f)
-         verticalLineToRelative(6.0f)
-         horizontalLineToRelative(-2.0f)
-         close()
-         moveTo(12.0f, 2.0f)
-         curveToRelative(-5.53f, 0.0f, -10.0f, 4.47f, -10.0f, 10.0f)
-         reflectiveCurveToRelative(4.47f, 10.0f, 10.0f, 10.0f)
-         reflectiveCurveToRelative(10.0f, -4.47f, 10.0f, -10.0f)
-         reflectiveCurveToRelative(-4.47f, -10.0f, -10.0f, -10.0f)
-         close()
-         moveTo(12.0f, 4.0f)
-         curveToRelative(4.42f, 0.0f, 8.0f, 3.58f, 8.0f, 8.0f)
-         reflectiveCurveToRelative(-3.58f, 8.0f, -8.0f, 8.0f)
-         reflectiveCurveToRelative(-8.0f, -3.58f, -8.0f, -8.0f)
-         reflectiveCurveToRelative(3.58f, -8.0f, 8.0f, -8.0f)
-         close()
+         exclamation()
+         outerCircle()
+         innerCircle()
       }
    }
 
-   return _error!!
+   return _outlinedError!!
 }
 
-private var _error: ImageVector? = null
+private var _outlinedError: ImageVector? = null
+
+@Suppress("UnusedReceiverParameter")
+val Icons.Filled.Error: ImageVector get() {
+   if (_filledError != null) { return _filledError!! }
+
+   _filledError = materialIcon("Filled.Error") {
+      materialPath {
+         exclamation()
+         outerCircle()
+      }
+   }
+
+   return _filledError!!
+}
+
+private var _filledError: ImageVector? = null
