884478a5accec9e04b605b807b999312dc8b2669
wcaokaze <wcaokaze@gmail.com>
2024-11-01 03:59:06 +0900

    一旦@Ignore外した

--- modules/mastodon/ui/src/androidUnitTest/kotlin/com/wcaokaze/probosqis/mastodon/ui/auth/urlinput/UrlInputPageTest.kt
+++ modules/mastodon/ui/src/androidUnitTest/kotlin/com/wcaokaze/probosqis/mastodon/ui/auth/urlinput/UrlInputPageTest.kt
@@ -264,7 +264,7 @@ class UrlInputPageTest {
       rule.onNodeWithText("https://example.wcaokaze.com/").assertIsEnabled()
    }
 
-   @Ignore("https://github.com/probosqis/probosqis/issues/82")
+   //@Ignore("https://github.com/probosqis/probosqis/issues/82")
    @Test
    fun screenshot_usual() {
       rule.setContent {
@@ -275,7 +275,7 @@ class UrlInputPageTest {
       rule.onRoot().captureRoboImage("urlInputPage/usual.png")
    }
 
-   @Ignore("https://github.com/probosqis/probosqis/issues/82")
+   //@Ignore("https://github.com/probosqis/probosqis/issues/82")
    @Test
    fun screenshot_loading() {
       val lock = ReentrantLock()
@@ -313,7 +313,7 @@ class UrlInputPageTest {
       }
    }
 
-   @Ignore("https://github.com/probosqis/probosqis/issues/82")
+   //@Ignore("https://github.com/probosqis/probosqis/issues/82")
    @Test
    fun screenshot_error() {
       val lock = ReentrantLock()
