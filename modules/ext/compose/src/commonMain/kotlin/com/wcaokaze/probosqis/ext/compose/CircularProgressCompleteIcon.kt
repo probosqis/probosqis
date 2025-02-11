187cdda1e1171ab826b738402f045e06d274a913
wcaokaze <wcaokaze@gmail.com>
2025-02-11 19:05:13 +0900

    CircularProgressCompleteIconにアニメーションを実装

--- modules/ext/compose/src/commonMain/kotlin/com/wcaokaze/probosqis/ext/compose/CircularProgressCompleteIcon.kt
+++ modules/ext/compose/src/commonMain/kotlin/com/wcaokaze/probosqis/ext/compose/CircularProgressCompleteIcon.kt
@@ -16,23 +16,60 @@
 
 package com.wcaokaze.probosqis.ext.compose
 
+import androidx.compose.animation.core.ExperimentalTransitionApi
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.rememberTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ProgressIndicatorDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import kotlin.time.Duration
+import kotlin.time.DurationUnit
+
+private val p1 = Offset(16.83f, 25.410f)
+private val p2 = Offset(21.00f, 29.585f)
+private val p3 = Offset(33.00f, 19.000f)
+
+private val d1 = (p2 - p1).getDistance()
+private val d2 = (p3 - p2).getDistance()
+private val totalD = d1 + d2
 
 @Composable
 fun CircularProgressCompleteIcon(
    modifier: Modifier = Modifier,
    tint: Color = ProgressIndicatorDefaults.circularColor,
    strokeWidth: Dp = ProgressIndicatorDefaults.CircularStrokeWidth,
+   animDelay: Duration = Duration.ZERO,
 ) {
+   val transitionState = remember {
+      MutableTransitionState(false).also { it.targetState = true }
+   }
+
+   @OptIn(ExperimentalTransitionApi::class)
+   val transition = rememberTransition(transitionState, label = "CircularProgressCompleteIcon")
+
+   val f by transition.animateFloat(
+      transitionSpec = {
+         tween(
+            durationMillis = 500,
+            delayMillis = animDelay.toInt(DurationUnit.MILLISECONDS),
+            easing = LinearOutSlowInEasing
+         )
+      },
+      targetValueByState = { if (it) { totalD } else { 0.0f } }
+   )
+
    Canvas(
       modifier = modifier
          .size(40.dp)
@@ -40,12 +77,49 @@ fun CircularProgressCompleteIcon(
       val xs = size.width  / 48.0f
       val ys = size.height / 48.0f
 
-      val p1 = Offset(16.83f * xs, 25.410f * ys)
-      val p2 = Offset(21.00f * xs, 29.585f * ys)
-      val p3 = Offset(33.00f * xs, 19.000f * ys)
-
       val strokeWidthPx = strokeWidth.toPx()
-      drawLine(tint, p1, p2, strokeWidthPx, cap = StrokeCap.Square)
-      drawLine(tint, p2, p3, strokeWidthPx, cap = StrokeCap.Square)
+
+      when {
+         f <= 0.0f -> {
+         }
+
+         f <= d1 -> {
+            val p1 = Offset(p1.x * xs, p1.y * ys)
+            val p2 = Offset(p2.x * xs, p2.y * ys)
+
+            drawLine(
+               color = tint,
+               start = p1,
+               end = p1 + (p2 - p1) * (f / d1),
+               strokeWidthPx,
+               StrokeCap.Square
+            )
+         }
+
+         f < totalD -> {
+            val p1 = Offset(p1.x * xs, p1.y * ys)
+            val p2 = Offset(p2.x * xs, p2.y * ys)
+            val p3 = Offset(p3.x * xs, p3.y * ys)
+
+            drawLine(color = tint, start = p1, end = p2, strokeWidthPx, StrokeCap.Square)
+
+            drawLine(
+               color = tint,
+               start = p2,
+               end = p2 + (p3 - p2) * ((f - d1) / d2),
+               strokeWidthPx,
+               StrokeCap.Square
+            )
+         }
+
+         else -> {
+            val p1 = Offset(p1.x * xs, p1.y * ys)
+            val p2 = Offset(p2.x * xs, p2.y * ys)
+            val p3 = Offset(p3.x * xs, p3.y * ys)
+
+            drawLine(color = tint, start = p1, end = p2, strokeWidthPx, StrokeCap.Square)
+            drawLine(color = tint, start = p2, end = p3, strokeWidthPx, StrokeCap.Square)
+         }
+      }
    }
 }
