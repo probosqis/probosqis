e395e9e8b807cb0906e07c8e32ee11f35e04710a
wcaokaze <wcaokaze@gmail.com>
2025-04-30 03:54:10 +0900

    added: CredentialRepository

---
+++ modules/credential/src/desktopMain/kotlin/com/wcaokaze/probosqis/credential/DesktopCredentialRepository.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2025 wcaokaze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.wcaokaze.probosqis.credential
+
+import com.wcaokaze.probosqis.panoptiqon.Cache
+import com.wcaokaze.probosqis.panoptiqon.TemporaryCacheApi
+import com.wcaokaze.probosqis.panoptiqon.loadCache
+import com.wcaokaze.probosqis.panoptiqon.saveCache
+import java.io.File
+import java.io.IOException
+
+class DesktopCredentialRepository(
+   allCredentialSerializers: List<CredentialRepository.CredentialSerializer<*>>,
+   directory: File
+) : AbstractCredentialRepository(allCredentialSerializers) {
+   private val dir = File(directory, "YeNl4QfY6KDSixTZ")
+      .also { dir ->
+         if (dir.exists()) {
+            require(dir.isDirectory)
+         } else {
+            if (!dir.mkdirs()) { throw IOException() }
+         }
+      }
+
+   private val credentialListFile = File(dir, "pI9mnCtxBMhYGJpJ")
+
+   /** @throws IOException */
+   @TemporaryCacheApi
+   override fun saveCredential(credential: Credential) {
+      val fileName = getFileNameFor(credential)
+      val file = File(dir, fileName)
+      saveCache(credential, file, json)
+
+      if (!credentialListFile.exists() || credentialListFile.length() == 0L) {
+         credentialListFile.writeText(fileName)
+      } else {
+         credentialListFile.appendText("\n" + fileName)
+      }
+   }
+
+   /** @throws IOException */
+   @TemporaryCacheApi
+   override fun loadAllCredentials(): List<Cache<Credential>> {
+      val fileNames = credentialListFile.readLines()
+
+      return fileNames.map {
+         val file = File(dir, it)
+         loadCache<Credential>(file, json).asCache()
+      }
+   }
+}
