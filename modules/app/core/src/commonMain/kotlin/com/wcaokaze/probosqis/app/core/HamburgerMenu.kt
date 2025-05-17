4b54b8715d54f74e5e138e3c8de46014ab5c5b9f
wcaokaze <wcaokaze@gmail.com>
2025-05-18 04:30:38 +0900

    ハンバーガーメニューにアカウント一覧を追加

--- modules/app/core/src/commonMain/kotlin/com/wcaokaze/probosqis/app/core/HamburgerMenu.kt
+++ modules/app/core/src/commonMain/kotlin/com/wcaokaze/probosqis/app/core/HamburgerMenu.kt
@@ -16,31 +16,94 @@
 
 package com.wcaokaze.probosqis.app.core
 
+import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import com.wcaokaze.probosqis.ext.compose.LoadState
+import com.wcaokaze.probosqis.foundation.credential.CredentialRepository
 import com.wcaokaze.probosqis.foundation.resources.Strings
+import com.wcaokaze.probosqis.mastodon.entity.Token
+import com.wcaokaze.probosqis.mastodon.repository.AppRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
 @Stable
-internal class HamburgerMenuState
+internal class HamburgerMenuState : KoinComponent {
+   private val credentialRepository: CredentialRepository by inject()
+   private val appRepository: AppRepository by inject()
+
+   var credentialLoadState: LoadState<List<Token>>
+      by mutableStateOf(LoadState.Loading)
+      private set
+
+   suspend fun fetchCredentials() {
+      credentialLoadState = withContext (Dispatchers.IO) {
+         try {
+            val credentials = credentialRepository.loadAllCredentials()
+               .map { credentialCache ->
+                  val credential = credentialCache.value as Token
+
+                  val credentialAccount
+                      = appRepository.getCredentialAccount(credential)
+
+                  credential.copy(
+                     account = credentialAccount,
+                  )
+               }
+
+            LoadState.Success(credentials)
+         } catch (e: Exception) {
+            LoadState.Error(e)
+         }
+      }
+   }
+}
 
 @Composable
 internal fun HamburgerMenu(
    state: HamburgerMenuState,
    onSettingItemClick: () -> Unit
 ) {
+   LaunchedEffect(Unit) {
+      state.fetchCredentials()
+   }
+
    ModalDrawerSheet {
-      Spacer(Modifier.weight(1f))
+      AccountList(
+         state.credentialLoadState,
+         modifier = Modifier
+            .fillMaxWidth()
+            .weight(1f)
+      )
 
       HorizontalDivider()
 
@@ -51,5 +114,79 @@ internal fun HamburgerMenu(
       )
 
       Spacer(Modifier.height(40.dp))
+   }
+}
+
+@Composable
+private fun AccountList(
+   credentialLoadState: LoadState<List<Token>>,
+   modifier: Modifier = Modifier
+) {
+   Crossfade(
+      credentialLoadState,
+      modifier = modifier
+   ) { state ->
+      when (state) {
+         is LoadState.Loading -> {
+            Box(Modifier.fillMaxSize()) {
+               CircularProgressIndicator(
+                  modifier = Modifier.align(Alignment.Center)
+               )
+            }
+         }
+         is LoadState.Success -> {
+            LazyColumn(
+               modifier = modifier
+            ) {
+               items(state.data) { token ->
+                  DropdownMenuItem(
+                     text = {
+                        Row(
+                           verticalAlignment = Alignment.CenterVertically
+                        ) {
+                           val credentialAccount = token.account!!.value
+                           val account = credentialAccount.account.value
+                           val username = account.username
+
+                           val displayName = account.displayName ?: account.username
+                           if (displayName != null) {
+                              Text(
+                                 displayName,
+                                 overflow = TextOverflow.Ellipsis,
+                                 maxLines = 1,
+                                 style = MaterialTheme.typography.titleMedium
+                              )
+                           }
+
+                           if (displayName != null && username != null) {
+                              Spacer(Modifier.width(4.dp))
+                           }
+
+                           if (username != null) {
+                              Text(
+                                 "@$username",
+                                 color = MaterialTheme.colorScheme.onSurfaceVariant,
+                                 overflow = TextOverflow.Ellipsis,
+                                 maxLines = 1,
+                                 style = MaterialTheme.typography.bodyMedium
+                              )
+                           }
+                        }
+                     },
+                     trailingIcon = {
+                        Icon(
+                           Icons.Default.KeyboardArrowDown,
+                           contentDescription = null
+                        )
+                     },
+                     onClick = {}
+                  )
+               }
+            }
+         }
+         is LoadState.Error -> {
+            Text("エラーだよ")
+         }
+      }
    }
 }
