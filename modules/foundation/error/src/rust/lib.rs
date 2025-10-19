c9ad904e3ffba37aa08adc0e61671be5ca613807
wcaokaze <wcaokaze@gmail.com>
2025-10-20 03:57:56 +0900

    modules:foundation:errorにRustクレートを追加

--- modules/foundation/error/src/rust/lib.rs
+++ modules/foundation/error/src/rust/lib.rs
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024-2025 wcaokaze
+ * Copyright 2025 wcaokaze
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,3 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-pub use foundation_error;
-pub use mastodon_repository;
-pub use nodeinfo_repository;
