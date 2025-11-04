51cdcc134e9cd6202d1923795ad4a2bf84955b02
wcaokaze <wcaokaze@gmail.com>
2025-11-05 03:16:53 +0900

    added: CacheSerializer, WritableCacheSerializer

---
+++ modules/ext/panoptiqon/src/jvmMain/kotlin/com/wcaokaze/probosqis/ext/panoptiqon/CacheSerializer.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2025 wcaokaze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.wcaokaze.probosqis.ext.panoptiqon
+
+import com.wcaokaze.probosqis.panoptiqon.AbstractCacheSerializer
+import com.wcaokaze.probosqis.panoptiqon.AbstractWritableCacheSerializer
+import com.wcaokaze.probosqis.panoptiqon.Cache
+import com.wcaokaze.probosqis.panoptiqon.WritableCache
+
+class CacheSerializer<T> : AbstractCacheSerializer<T>() {
+   override fun loadCache(cacheId: Cache.Id): Cache<T> {
+      @Suppress("UNCHECKED_CAST")
+      return Panoptiqon.loadById(cacheId).asCache() as Cache<T>
+   }
+}
+
+class WritableCacheSerializer<T> : AbstractWritableCacheSerializer<T>() {
+   override fun loadCache(cacheId: Cache.Id): WritableCache<T> {
+      @Suppress("UNCHECKED_CAST")
+      return Panoptiqon.loadById(cacheId) as WritableCache<T>
+   }
+}
